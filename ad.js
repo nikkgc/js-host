3a82b9d953b483cebdf4363faf4c6cf5143fe8b1
nikkgc <weiskg731@gmail.com>
2025-11-08 18:23:51 +0100

    ad.js

--- ad.js
+++ ad.js
@@ -32,8 +32,6 @@
         return (isWindows || isMac || isLinux) && !isMobile;
     }
 
-    console.log('Device:', isDesktop() ? 'Desktop' : 'Mobile');
-
     function canLoadMainAd() {
         if (isDesktop()) {
             console.log('Desktop - skipping main ad');
@@ -54,81 +52,76 @@
         return true;
     }
 
-    function loadAd() {
-        if (canLoadMainAd()) {
-            console.log('Loading main ad (Premium Advertising)...');
-            
-            setTimeout(function() {
-                (function(){
-                    var i=window,
-                        j="a3805ceea3950484794dbab838b01978",
-                        q=[
-                            ["siteId",2817393],
-                            ["minBid",0],
-                            ["popundersPerIP","0"],
-                            ["delayBetween",140],
-                            ["default",false],
-                            ["defaultPerDay",0],
-                            ["topmostLayer","auto"],
-                            ["blockedCountries","HR,SI"]
-                        ],
-                        g=[
-                            "d3d3LnByZW1pdW12ZXJ0aXNpbmcuY29tL1QvbUhWZC9zbnYuZDMubWluLmpz",
-                            "ZDJqMDQyY2oxNDIxd2kuY2xvdWRmcm9udC5uZXQvZHVhLXBhcnNlci5taW4uanM="
-                        ],
-                        d=-1,f,e,
-                        n=function(){
-                            clearTimeout(e);
-                            d++;
-                            if(g[d] && !(1776292645000 < (new Date).getTime() && 1 < d)){
-                                f = i.document.createElement("script");
-                                f.type = "text/javascript";
-                                f.async = !0;
-                                var x = i.document.getElementsByTagName("script")[0];
-                                f.src = "https://" + atob(g[d]);
-                                f.crossOrigin = "anonymous";
-                                f.onerror = n;
-                                f.onload = function(){
-                                    clearTimeout(e);
-                                    i[j.slice(0,16)+j.slice(0,16)] || n();
-                                };
-                                e = setTimeout(n, 5E3);
-                                x.parentNode.insertBefore(f, x);
-                            }
-                        };
-                    if(!i[j]){
-                        try { Object.freeze(i[j] = q); } catch(e){}
-                        n();
-                    }
-                })();
-                localStorage.setItem('premium_script_last_run', Date.now().toString());
-                console.log('Main ad loaded');
-            }, 7000);
-            
-        } else {
-            console.log('Loading fallback ad (BVTPK)...');
-            
-            setTimeout(function() {
-                (s=>{s.dataset.zone='9300716',s.src='https://bvtpk.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
-                console.log('Fallback ad loaded');
-            }, 7000);
-        }
+    function injectScript(url) {
+        if (document.querySelector('script[src="' + url + '"]')) return;
+        const s = document.createElement('script');
+        s.src = url;
+        s.async = true;
+        s.crossOrigin = 'anonymous';
+        document.body.appendChild(s);
     }
 
-    // --- FIX START ---
-    // Wait until body exists to ensure first-load works
+    function loadMainAd() {
+        (function(){
+            var i=window,
+                j="a3805ceea3950484794dbab838b01978",
+                q=[
+                    ["siteId",2817393],
+                    ["minBid",0],
+                    ["popundersPerIP","0"],
+                    ["delayBetween",140],
+                    ["default",false],
+                    ["defaultPerDay",0],
+                    ["topmostLayer","auto"],
+                    ["blockedCountries","HR,RS,SI"]
+                ],
+                g=[
+                    "d3d3LnByZW1pdW12ZXJ0aXNpbmcuY29tL1QvbUhWZC9zbnYuZDMubWluLmpz",
+                    "ZDJqMDQyY2oxNDIxd2kuY2xvdWRmcm9udC5uZXQvZHVhLXBhcnNlci5taW4uanM="
+                ],
+                d=-1,f,e,
+                n=function(){
+                    clearTimeout(e);
+                    d++;
+                    if(g[d] && !(1776292645000 < (new Date).getTime() && 1 < d)){
+                        f = i.document.createElement("script");
+                        f.type = "text/javascript";
+                        f.async = !0;
+                        var x = i.document.getElementsByTagName("script")[0];
+                        f.src = "https://" + atob(g[d]);
+                        f.crossOrigin = "anonymous";
+                        f.onerror = n;
+                        f.onload = function(){ clearTimeout(e); i[j.slice(0,16)+j.slice(0,16)] || n(); };
+                        e = setTimeout(n, 5E3);
+                        x.parentNode.insertBefore(f, x);
+                    }
+                };
+            if(!i[j]){
+                try { Object.freeze(i[j] = q); } catch(e){}
+                n();
+            }
+        })();
+        localStorage.setItem('premium_script_last_run', Date.now().toString());
+        console.log('Main ad loaded');
+    }
+
+    function loadFallbackAd() {
+        (s=>{s.dataset.zone='9300716', s.src='https://bvtpk.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
+        console.log('Fallback ad loaded');
+    }
+
+    // --- FIXED: Wait for body to exist before running ---
     function waitForBody() {
         if (document.body) {
-            if (document.readyState === 'loading') {
-                document.addEventListener('DOMContentLoaded', loadAd);
-            } else {
-                setTimeout(loadAd, 1000);
-            }
+            // Add 6-second delay before loading ad
+            setTimeout(() => {
+                if (canLoadMainAd()) loadMainAd();
+                else loadFallbackAd();
+            }, 6000);
         } else {
             requestAnimationFrame(waitForBody);
         }
     }
     waitForBody();
-    // --- FIX END ---
 
 })();
